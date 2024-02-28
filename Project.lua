3ffaa5246b4f91d5ccb7fddfddbc287383b319b2
rrhiemstar <rrhiemstar@gmail.com>
2024-02-28 06:37:07 +0100

    <release> PkgB..v0.1.1

--- Project.lua
+++ Project.lua
@@ -1,8 +1,9 @@
 Project = {
-    name = "PkgB",
-    uuid = "1821bd21-e70d-44b5-a834-da5ae9cf4cee",
-    authors = {"rrhiemstar<rrhiemstar@gmail.com>"},
-    version = "0.1.0",
-    deps = {}
+  name = "PkgB",
+  uuid = "1821bd21-e70d-44b5-a834-da5ae9cf4cee",
+  authors = {"rrhiemstar<rrhiemstar@gmail.com>", }, 
+  version = "0.1.1",
+  deps = {
+    },
 }
 return Project
